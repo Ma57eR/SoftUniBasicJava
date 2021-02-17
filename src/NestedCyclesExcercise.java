24d4f1f34117d352fc954a555382aa5f2f96d6ea
georgi.georgiev <ggeorgiev83@gmail.com>
2021-02-17 17:29:18 +0200

    17022021

---
+++ src/NestedCyclesExcercise.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NestedCyclesExcercise {
+    public static void main(String[] args) {
+
+
+        //01. Number Pyramid
+
+//        Scanner scan = new Scanner(System.in);
+//        int num = Integer.parseInt(scan.nextLine());
+//        int counter = 0;
+//        for (int row = 1; row <= num; row++) {
+//
+//            for (int col = 1; col <= row; col++) {
+//
+//                counter++;
+//                System.out.print(counter + " ");
+//                if (counter == num) {
+//                    break;
+//                }
+//            }
+//            System.out.println();
+//            if (counter == num) {
+//                break;
+//            }
+//        }
+
+
+
+
+    }
+
+}
