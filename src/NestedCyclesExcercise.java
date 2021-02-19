cd7cf0153cd0ee48d761eefcefda0b565dd3367e
georgi.georgiev <ggeorgiev83@gmail.com>
2021-02-19 17:07:43 +0200

    19022021

--- src/NestedCyclesExcercise.java
+++ src/NestedCyclesExcercise.java
@@ -25,6 +25,44 @@ public class NestedCyclesExcercise {
 //            }
 //        }
 
+        //02. Equal Sums Even Odd Position
+
+        Scanner scan = new Scanner(System.in);
+
+        int min = Integer.parseInt(scan.nextLine());
+        int max = Integer.parseInt(scan.nextLine());
+        int length = String.valueOf(min).length();
+
+
+
+
+
+
+        for (int i = min; i <= max; i++) {
+            int counter = 1;
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int j = 0; j < length; j++) {
+                String ch = String.valueOf(i);
+                char symbol = ch.charAt(j);
+                int num = Character.getNumericValue(symbol);
+
+
+                //System.out.println(symbol);
+                if (counter % 2 != 0) {
+                    sumOdd += num;
+                } else {
+                    sumEven += num;
+                }
+                counter++;
+            }
+            if (sumEven == sumOdd) {
+                System.out.printf("%d ",i);
+            }
+
+        }
+
+
 
 
 
