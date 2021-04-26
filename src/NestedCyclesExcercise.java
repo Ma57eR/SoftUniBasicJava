92f2f04c4fab126d7bb595a15f5829c9a700b09d
georgi.georgiev <ggeorgiev83@gmail.com>
2021-04-26 17:08:12 +0300

    26042021

--- src/NestedCyclesExcercise.java
+++ src/NestedCyclesExcercise.java
@@ -27,39 +27,62 @@ public class NestedCyclesExcercise {
 
         //02. Equal Sums Even Odd Position
 
+//        Scanner scan = new Scanner(System.in);
+//
+//        int min = Integer.parseInt(scan.nextLine());
+//        int max = Integer.parseInt(scan.nextLine());
+//        int length = String.valueOf(min).length();
+//
+//
+//
+//
+//
+//
+//        for (int i = min; i <= max; i++) {
+//            int counter = 1;
+//            int sumEven = 0;
+//            int sumOdd = 0;
+//            for (int j = 0; j < length; j++) {
+//                String ch = String.valueOf(i);
+//                char symbol = ch.charAt(j);
+//                int num = Character.getNumericValue(symbol);
+//
+//
+//                //System.out.println(symbol);
+//                if (counter % 2 != 0) {
+//                    sumOdd += num;
+//                } else {
+//                    sumEven += num;
+//                }
+//                counter++;
+//            }
+//            if (sumEven == sumOdd) {
+//                System.out.printf("%d ",i);
+//            }
+//
+//        }
+
+
         Scanner scan = new Scanner(System.in);
-
-        int min = Integer.parseInt(scan.nextLine());
-        int max = Integer.parseInt(scan.nextLine());
-        int length = String.valueOf(min).length();
+        int num = Integer.parseInt(scan.nextLine());
+        int count = 0;
 
 
-
-
-
-
-        for (int i = min; i <= max; i++) {
-            int counter = 1;
-            int sumEven = 0;
-            int sumOdd = 0;
-            for (int j = 0; j < length; j++) {
+        for (int i = 1111; i <= 9999; i++) {
+            for (int j = 0; j <= 3; j++) {
                 String ch = String.valueOf(i);
                 char symbol = ch.charAt(j);
-                int num = Character.getNumericValue(symbol);
+                int num2 = Character.getNumericValue(symbol);
+                if (num2 !=0) {
 
-
-                //System.out.println(symbol);
-                if (counter % 2 != 0) {
-                    sumOdd += num;
-                } else {
-                    sumEven += num;
+                if (num % num2 == 0) {
+                    count++;
                 }
-                counter++;
+                }
             }
-            if (sumEven == sumOdd) {
-                System.out.printf("%d ",i);
-            }
-
+            if (count == 4) {
+                System.out.printf("%d ", i);
+            } count = 0;
         }
 
 
---
+++ src/NestedCyclesMoreExcercise.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NestedCyclesMoreExcercise {
+
+    public static void main(String[] args) {
+
+
+        //01. Unique PIN Codes
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+    }
+}
