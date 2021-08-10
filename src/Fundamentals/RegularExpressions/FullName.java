0d278c12a70855ad7da20e2a609194d6deb812c0
Ma57eR <ggeorgiev83@gmail.com>
2021-08-10 22:50:36 +0300

    25072021

--- src/Fundamentals/RegularExpressions/FullName.java
+++ src/Fundamentals/RegularExpressions/FullName.java
@@ -1,29 +1,22 @@
 package Fundamentals.RegularExpressions;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchPhoneNumber {
+public class FullName {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String regex = "\\+[359]+([\\- ])2\\1[0-9]{3}\\1\\d{4}\\b";
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
         String input = scan.nextLine();
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
-        List<String> matchedPhones = new ArrayList<>();
-
         while (matcher.find()) {
-            matchedPhones.add(matcher.group().toString());
+            System.out.print(matcher.group() + " ");
         }
-
-        System.out.println(String.join(", ", matchedPhones));
-
 
     }
 }
--- src/Fundamentals/RegularExpressions/MatchDates.java
+++ src/Fundamentals/RegularExpressions/MatchDates.java
@@ -1,29 +1,19 @@
 package Fundamentals.RegularExpressions;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchPhoneNumber {
+public class MatchDates {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String regex = "\\+[359]+([\\- ])2\\1[0-9]{3}\\1\\d{4}\\b";
+        String regex = "\\b(?<day>\\d{2})(?<separator>[\\/\\-\\.])(?<Month>[A-z]+)\\2(?<year>\\d{4}\\b)";
         String input = scan.nextLine();
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
-        List<String> matchedPhones = new ArrayList<>();
-
-        while (matcher.find()) {
-            matchedPhones.add(matcher.group().toString());
-        }
-
-        System.out.println(String.join(", ", matchedPhones));
-
-
+        while ()
     }
 }
